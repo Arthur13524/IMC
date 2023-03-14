9ae822916d3979c65a01b886bfeef98722872832
Arthur13524 <arthurfeliciano271@gmail.com>
2023-03-15 02:43:07 +0900

    aaa

--- www/components/motor.js
+++ www/components/motor.js
@@ -12,4 +12,16 @@ function calcularconsumo() {
  else if((consumo > 18.5) && (consumo <= 24.9)) {
      document.getElementById('nome').textContent = consumo + " Você está no peso ideal!" 
  }
+ else if((consumo > 24.9) && (consumo <= 29.9)) {
+     document.getElementById('nome').textContent = consumo + " Você está ligeiramente acima do peso!" 
+ }
+ else if((consumo > 29.9) && (consumo <= 34.9)) {
+     document.getElementById('nome').textContent = consumo + " Você está com obesidade grau I!" 
+ }
+ else if((consumo > 34.9) && (consumo <= 39.9)) {
+     document.getElementById('nome').textContent = consumo + " Você está com obesidade grau II!" 
+ }
+ else {
+     document.getElementById('nome').textContent = consumo + " Você está com obesidade grau III!" 
+ }
 }
